33f8d5d06da8dca941d5e0c1242597e077ee5d59
saurabhpaliwal55 <75921538+saurabhpaliwal55@users.noreply.github.com>
2025-03-22 14:17:23 +0530

    fix: remove unused root route handler

--- backend/app.js
+++ backend/app.js
@@ -9,11 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 
-app.get("/",(req,res)=>{
-    res.send("Hello from server");
-})
-
-
 import userRouter from "./routes/register.route.js"
 
 app.use("/api/user",userRouter);
