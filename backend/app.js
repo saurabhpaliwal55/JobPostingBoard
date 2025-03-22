9fbfe82ef9d0905c3689cde44ca2599f7a26ef9d
saurabhpaliwal55 <75921538+saurabhpaliwal55@users.noreply.github.com>
2025-03-22 14:26:38 +0530

    buld

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,10 @@ import userRouter from "./routes/register.route.js";
 app.use("/api/user", userRouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
+  res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
 );
 
 export { app };
