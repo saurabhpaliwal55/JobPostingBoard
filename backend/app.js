0f479d278671a487819e4110889ca1451272c315
saurabhpaliwal55 <75921538+saurabhpaliwal55@users.noreply.github.com>
2025-03-22 13:40:30 +0530

    fix: path

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cookieParser from 'cookie-parser'
-
+import path from "path";
 
 
 const app = express();
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
-// import dotenv from "dotenv";
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
 dotenv.config();
 import { app } from "./app.js";
 import { connectDB } from "./connectDB.js";
 
-// dotenv.config();
 const PORT = process.env.PORT || 3030;
 
 connectDB().then(() => {
